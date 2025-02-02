bd1bb210ea7c16656f344b4fe0320cf3ba51090d
Prakhar Singh <sprakhar606@gmail.com>
2025-02-02 20:02:23 +0530

    Create Demo.java
    new 1

---
+++ Core Java/Demo.java
@@ -0,0 +1,17 @@
+   class Demo
+{
+ //java uses camelCase notation
+/* multiline comment */
+public static void main(String[] args)// method
+{
+  
+        System.out.print("Hello java"); 
+
+
+}
+
+
+
+
+
+}
